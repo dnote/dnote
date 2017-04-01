b89ecab1274373f7cf0307d0fc2e121e365951b6
Sung Won Cho <mikeswcho@gmail.com>
2017-04-01 14:53:08 +1100

    Add install script

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # Dnote CLI
 
-A command line interface for spontaneously recording and owning your developer journey
+A command line interface for spontaneously capturing the things you learn while coding
 
-## WIP
+## Installation
+
+On macOS, or Linux, run:
+
+    curl https://raw.githubusercontent.com/dnote-io/cli/master/install.sh | sh
+
+On Windows, download [binary](https://github.com/dnote-io/cli/releases)
+
+## Links
+
+* [Website](https://dnote.io)
 
 ## License
 
---
+++ install.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+not_supported() {
+  echo "Sorry, OS is not supported: ${UNAME}. Please compile manually from https://github.com/dnote-io/cli"
+  exit 1
+}
+
+install() {
+  set -eu
+
+  UNAME=$(uname)
+
+  if [ "$UNAME" != "Linux" -a "$UNAME" != "Darwin" ] ; then
+    not_supported
+  fi
+
+  if [ "$UNAME" = "Darwin" ]; then
+    OSX_ARCH=$(uname -m)
+    if [ "${OSX_ARCH}" = "x86_64" ]; then
+      PLATFORM="darwin-amd64"
+    else
+      not_supported
+    fi
+  elif [ "$UNAME" = "Linux" ]; then
+    LINUX_ARCH=$(uname -m)
+    if [ "${LINUX_ARCH}" = "x86_64" ]; then
+      PLATFORM="linux-amd64"
+    else
+      not_supported
+    fi
+  fi
+
+  LATEST=$(curl -s https://api.github.com/repos/dnote-io/cli/tags | grep -Eo '"name":.*[^\\]",'  | head -n 1 | sed 's/[," ]//g' | cut -d ':' -f 2)
+  URL="https://github.com/dnote-io/cli/releases/download/$LATEST/dnote_$PLATFORM"
+  DEST=${DEST:-/usr/local/bin/dnote}
+
+  if [ -z $LATEST ]; then
+    echo "Error fetching. Please try again."
+    exit 1
+  else
+    echo "Download Dnote binary from curl https://github.com/dnote-io/cli/releases/download/$LATEST/dnote-$PLATFORM to $DEST"
+    if curl -sL https://github.com/dnote-io/cli/releases/download/$LATEST/dnote-$PLATFORM -o $DEST; then
+      chmod +x $DEST
+      echo "Dnote installation was successful"
+    else
+      echo "Installation failed"
+    fi
+  fi
+}
+
+install
+
