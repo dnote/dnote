c1ba1ba5c95480602f5c059499f424150c362906
YKC <yiikuochong@gmail.com>
2017-04-12 18:26:23 +0800

    Update

--- cmd/edit/edit.go
+++ cmd/edit/edit.go
@@ -2,18 +2,56 @@ package edit
 
 import (
 	"fmt"
-	"github.com/dnote-io/cli/utils"
+
+	"io/ioutil"
+    "strings"
+
+	"../../utils"
 )
 
-func Edit(notename string, newcontent string) error{
-	book, book_err := utils.GetCurrentBook()
-	json_data, json_err := utils.GetDnote()
+func replaceLine(replace_data string, target_line string) error {
+	path, err := utils.GetDnotePath()
 
-	if book_err != nil {return book_err}
-	if json_err != nil {return json_err}
+	if err != nil {
+		return err
+	}
 
-	fmt.Println(book)
-	fmt.Println(json_data)
+	input, err := ioutil.ReadFile(path)
+
+	if err != nil {
+		return nil
+	}
+
+	lines := strings.Split(string(input), "\n")
+
+	for i, line := range lines {
+		if strings.Contains(line, target_line) {
+			lines[i] = replace_data
+		}
+	}
+
+	return nil
+}
+
+func Edit(notename string, newcontent string) error {
+	book, err := utils.GetCurrentBook()
+
+	if err != nil {
+		return err
+	}
+
+	json_data, err := utils.GetDnote()
+
+	if err != nil {
+		return err
+	}
+
+	for _, note := range json_data[book] {
+		if note.Name == notename {
+			note.Content = newcontent
+			replaceLine(note.Content, "Content")
+		}
+	}
 
 	return nil 
 }
--- cmd/new/new.go
+++ cmd/new/new.go
@@ -6,7 +6,7 @@ import (
 	"io/ioutil"
 	"time"
 
-	"github.com/dnote-io/cli/utils"
+	"../../utils"
 )
 
 func Run(notename string, content string) error {
