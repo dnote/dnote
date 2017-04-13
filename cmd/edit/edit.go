c6233004ba4b446b0168a1077b600d65d3c51393
YKC <yiikuochong@gmail.com>
2017-04-13 16:01:32 +0800

    Update

--- cmd/edit/edit.go
+++ cmd/edit/edit.go
@@ -3,53 +3,34 @@ package edit
 import (
 	"fmt"
 
-	"io/ioutil"
-    "strings"
-
+	"encoding/json"
 	"../../utils"
 )
 
-func replaceLine(replace_data string, target_line string) error {
-	path, err := utils.GetDnotePath()
-
-	if err != nil {
-		return err
-	}
-
-	input, err := ioutil.ReadFile(path)
-
-	if err != nil {
-		return nil
-	}
-
-	lines := strings.Split(string(input), "\n")
-
-	for i, line := range lines {
-		if strings.Contains(line, target_line) {
-			lines[i] = replace_data
-		}
-	}
-
-	return nil
-}
-
 func Edit(notename string, newcontent string) error {
+	// Get the current book.
 	book, err := utils.GetCurrentBook()
-
 	if err != nil {
 		return err
 	}
 
 	json_data, err := utils.GetDnote()
-
 	if err != nil {
 		return err
 	}
 
 	for _, note := range json_data[book] {
+
 		if note.Name == notename {
 			note.Content = newcontent
-			replaceLine(note.Content, "Content")
+			out, err := json.Marshal(line)
+			if err != nil {
+				return err
+			}
+		
+			note_data := "[" + string(out) + "]"
+			json_data[book] = note_data // Assigns a new data to json map, does nto work (error: cannot use note_data (type string) as type utils.Book in assignment)
+			fmt.Println(json_data[book])
 		}
 	}
 
