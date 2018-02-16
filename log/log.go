a7e7364332d1d93e05739a9b76ec562a2d8e478e
Daniel Hume <daniel@thehumes.at>
2018-02-17 00:53:30 +0100

    Fix windows console colours, include installer script for windows (#73)
    * partial fix of ls and add commands
    
    * completing log changes for windows-compatible colours
    
    * Added Windows installer batch script
    
    * cleanup
    
    * ran dep ensure

--- log/log.go
+++ log/log.go
@@ -2,32 +2,34 @@ package log
 
 import (
 	"fmt"
+
+	"github.com/fatih/color"
 )
 
 var (
-	ColorRed    = 31
-	ColorGreen  = 32
-	ColorYellow = 33
-	ColorBlue   = 34
-	ColorGray   = 37
+	SprintfRed    = color.New(color.FgRed).SprintfFunc()
+	SprintfGreen  = color.New(color.FgGreen).SprintfFunc()
+	SprintfYellow = color.New(color.FgYellow).SprintfFunc()
+	SprintfBlue   = color.New(color.FgBlue).SprintfFunc()
+	SprintfGray   = color.New(color.FgWhite).SprintfFunc()
 )
 
 var indent = "  "
 
 func Info(msg string) {
-	fmt.Printf("%s\033[%dm%s\033[0m %s\n", indent, ColorBlue, "•", msg)
+	fmt.Fprintf(color.Output, "%s %s %s\n", indent, SprintfBlue("•"), msg)
 }
 
 func Infof(msg string, v ...interface{}) {
-	fmt.Printf("%s\033[%dm%s\033[0m %s", indent, ColorBlue, "•", fmt.Sprintf(msg, v...))
+	fmt.Fprintf(color.Output, "%s %s %s", indent, SprintfBlue("•"), fmt.Sprintf(msg, v...))
 }
 
 func Success(msg string) {
-	fmt.Printf("%s\033[%dm%s\033[0m %s", indent, ColorGreen, "✔", msg)
+	fmt.Fprintf(color.Output, "%s%s %s", indent, SprintfGreen("✔"), msg)
 }
 
 func Successf(msg string, v ...interface{}) {
-	fmt.Printf("%s\033[%dm%s\033[0m %s", indent, ColorGreen, "✔", fmt.Sprintf(msg, v...))
+	fmt.Fprintf(color.Output, "%s%s %s", indent, SprintfGreen("✔"), fmt.Sprintf(msg, v...))
 }
 
 func Plain(msg string) {
@@ -35,21 +37,17 @@ func Plain(msg string) {
 }
 
 func Plainf(msg string, v ...interface{}) {
-	fmt.Printf("%s%s", indent, fmt.Sprintf(msg, v...))
+	fmt.Fprintf(color.Output, "%s%s", indent, fmt.Sprintf(msg, v...))
 }
 
 func Warnf(msg string, v ...interface{}) {
-	fmt.Printf("%s\033[%dm%s\033[0m %s", indent, ColorRed, "•", fmt.Sprintf(msg, v...))
+	fmt.Fprintf(color.Output, "%s%s %s", indent, SprintfRed("•"), fmt.Sprintf(msg, v...))
 }
 
 func Error(msg string) {
-	fmt.Printf("%s\033[%dm%s\033[0m %s\n", indent, ColorRed, "⨯", msg)
+	fmt.Fprintf(color.Output, "%s%s %s\n", indent, SprintfRed("⨯"), msg)
 }
 
 func Printf(msg string, v ...interface{}) {
-	fmt.Printf("%s\033[%dm%s\033[0m %s", indent, ColorGray, "•", fmt.Sprintf(msg, v...))
-}
-
-func WithPrefixf(prefixColor int, prefix, msg string, v ...interface{}) {
-	fmt.Printf("  \033[%dm%s\033[0m %s\n", prefixColor, prefix, fmt.Sprintf(msg, v...))
+	fmt.Fprintf(color.Output, "%s%s %s", indent, SprintfGray("•"), fmt.Sprintf(msg, v...))
 }
