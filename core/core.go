53c470ad3a4bf0876566d532077965fdb5ebee43
Sung Won Cho <mikeswcho@gmail.com>
2018-02-17 11:37:41 +1100

    0.2.1

--- core/core.go
+++ core/core.go
@@ -18,7 +18,7 @@ import (
 
 const (
 	// Version is the current version of dnote
-	Version = "0.2.0"
+	Version = "0.2.1"
 
 	// TimestampFilename is the name of the file containing upgrade info
 	TimestampFilename = "timestamps"
