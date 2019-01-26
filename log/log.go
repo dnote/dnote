faed1e5444cbea29da010e7b99e69d15dcd73e2d
Sung Won Cho <mikeswcho@gmail.com>
2019-01-26 12:57:02 +1100

    Revert "Fix windows console colours, include installer script for windows (#73)"
    This reverts commit a7e7364332d1d93e05739a9b76ec562a2d8e478e.

--- log/log.go
+++ log/log.go
@@ -3,34 +3,32 @@ package log
 import (
 	"fmt"
 	"os"
-
-	"github.com/fatih/color"
 )
 
 var (
-	SprintfRed    = color.New(color.FgRed).SprintfFunc()
-	SprintfGreen  = color.New(color.FgGreen).SprintfFunc()
-	SprintfYellow = color.New(color.FgYellow).SprintfFunc()
-	SprintfBlue   = color.New(color.FgBlue).SprintfFunc()
-	SprintfGray   = color.New(color.FgWhite).SprintfFunc()
+	ColorRed    = 31
+	ColorGreen  = 32
+	ColorYellow = 33
+	ColorBlue   = 34
+	ColorGray   = 37
 )
 
 var indent = "  "
 
 func Info(msg string) {
-	fmt.Fprintf(color.Output, "%s%s %s", indent, SprintfBlue("•"), msg)
+	fmt.Printf("%s\033[%dm%s\033[0m %s\n", indent, ColorBlue, "•", msg)
 }
 
 func Infof(msg string, v ...interface{}) {
-	fmt.Fprintf(color.Output, "%s%s %s", indent, SprintfBlue("•"), fmt.Sprintf(msg, v...))
+	fmt.Printf("%s\033[%dm%s\033[0m %s", indent, ColorBlue, "•", fmt.Sprintf(msg, v...))
 }
 
 func Success(msg string) {
-	fmt.Fprintf(color.Output, "%s%s %s", indent, SprintfGreen("✔"), msg)
+	fmt.Printf("%s\033[%dm%s\033[0m %s", indent, ColorGreen, "✔", msg)
 }
 
 func Successf(msg string, v ...interface{}) {
-	fmt.Fprintf(color.Output, "%s%s %s", indent, SprintfGreen("✔"), fmt.Sprintf(msg, v...))
+	fmt.Printf("%s\033[%dm%s\033[0m %s", indent, ColorGreen, "✔", fmt.Sprintf(msg, v...))
 }
 
 func Plain(msg string) {
@@ -38,28 +36,28 @@ func Plain(msg string) {
 }
 
 func Plainf(msg string, v ...interface{}) {
-	fmt.Fprintf(color.Output, "%s%s", indent, fmt.Sprintf(msg, v...))
+	fmt.Printf("%s%s", indent, fmt.Sprintf(msg, v...))
 }
 
 func Warnf(msg string, v ...interface{}) {
-	fmt.Fprintf(color.Output, "%s%s %s", indent, SprintfRed("•"), fmt.Sprintf(msg, v...))
+	fmt.Printf("%s\033[%dm%s\033[0m %s", indent, ColorRed, "•", fmt.Sprintf(msg, v...))
 }
 
 func Error(msg string) {
-	fmt.Fprintf(color.Output, "%s%s %s", indent, SprintfRed("⨯"), msg)
-}
-
-func Errorf(msg string, v ...interface{}) {
-	fmt.Fprintf(color.Output, "%s%s %s", indent, SprintfRed("⨯"), fmt.Sprintf(msg, v...))
+	fmt.Printf("%s\033[%dm%s\033[0m %s\n", indent, ColorRed, "⨯", msg)
 }
 
 func Printf(msg string, v ...interface{}) {
-	fmt.Fprintf(color.Output, "%s%s %s", indent, SprintfGray("•"), fmt.Sprintf(msg, v...))
+	fmt.Printf("%s\033[%dm%s\033[0m %s", indent, ColorGray, "•", fmt.Sprintf(msg, v...))
+}
+
+func WithPrefixf(prefixColor int, prefix, msg string, v ...interface{}) {
+	fmt.Printf("  \033[%dm%s\033[0m %s\n", prefixColor, prefix, fmt.Sprintf(msg, v...))
 }
 
 // Debug prints to the console if DNOTE_DEBUG is set
 func Debug(msg string, v ...interface{}) {
 	if os.Getenv("DNOTE_DEBUG") == "1" {
-		fmt.Fprintf(color.Output, "%s %s", SprintfGray("DEBUG:"), fmt.Sprintf(msg, v...))
+		fmt.Printf("%s %s", "DEBUG:", fmt.Sprintf(msg, v...))
 	}
 }
