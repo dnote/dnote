15572981ed8cda824192aca430976b8e7e45da89
Sung Won Cho <mikeswcho@gmail.com>
2019-03-23 16:23:08 +1100

    Improve test

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-
 # run_server_test.sh runs server test files sequentially
 # https://stackoverflow.com/questions/23715302/go-how-to-run-tests-for-multiple-packages
 
+set -e
+
 # clear tmp dir in case not properly torn down
-rm -rf ./tmp
+rm -rf $GOPATH/src/github.com/dnote/cli/tmp
 
 # run test
+pushd $GOPATH/src/github.com/dnote/cli
+
 go test ./... \
   -p 1\
   --tags "fts5"
+
+popd
