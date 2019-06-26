75d1cb42e86d424b4096d8b10d78667c091edd41
Sung Won Cho <sung@monomax.sh>
2019-06-26 18:43:39 +1000

    Deps (#208)
    * Consolidate deps
    
    * Fix CI

--- pkg/server/api/scripts/test-local.sh
+++ pkg/server/api/scripts/test-local.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+# shellcheck disable=SC1090
 # test-local.sh runs api tests using local setting
 set -eux
 
 basePath=$GOPATH/src/github.com/dnote/dnote/pkg/server/api
 
-export $(cat "$basePath"/.env.test | xargs)
+
+set -a
+source "$basePath"/.env.test
+set +a
+
 "$basePath"/scripts/test.sh
