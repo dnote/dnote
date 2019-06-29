ed02f185f025c337f8eafb57eacba364030148cd
Sung Won Cho <sung@monomax.sh>
2019-06-29 12:25:02 +1000

    Check if sql-migrate exists

--- pkg/server/database/scripts/create-migration.sh
+++ pkg/server/database/scripts/create-migration.sh
@@ -3,6 +3,15 @@
 # server side Postgres database using the sql-migrate tool.
 set -eux
 
+is_command () {
+  command -v "$1" >/dev/null 2>&1;
+}
+
+if ! is_command sql-migrate; then
+  echo "sql-migrate is not found. Please run install-sql-migrate.sh"
+  exit 1
+fi
+
 if [ "$#" == 0 ]; then
   echo "filename not provided"
   exit 1
