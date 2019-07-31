ff0006dad34ffd5ddc0576bfcde2a513e970782c
Sung Won Cho <sung@monomax.sh>
2019-07-31 13:09:57 +1000

    Add missing licenses

--- web/scripts/placeholder.js
+++ web/scripts/placeholder.js
@@ -1,4 +1,20 @@
-#!/usr/bin/env node
+/* Copyright (C) 2019 Monomax Software Pty Ltd
+ *
+ * This file is part of Dnote.
+ *
+ * Dnote is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Dnote is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Dnote.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 // placeholder.js replaces the placeholders in index.html with real values
 // It is needed to load assets whose paths are not fixed because they change
