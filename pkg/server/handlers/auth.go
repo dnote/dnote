b50530b135141be9da84f835ae0d6cdda1299fc0
Sung Won Cho <sung@monomax.sh>
2019-11-29 18:19:07 +0800

    Fix email type

--- pkg/server/handlers/auth.go
+++ pkg/server/handlers/auth.go
@@ -137,7 +137,7 @@ func (a *App) createResetToken(w http.ResponseWriter, r *http.Request) {
 		HandleError(w, errors.Wrap(err, "executing reset password email template").Error(), nil, http.StatusInternalServerError)
 	}
 
-	if err := a.EmailBackend.Queue("Reset your password", "sung@getdnote.com", []string{params.Email}, "text/html", body); err != nil {
+	if err := a.EmailBackend.Queue("Reset your password", "sung@getdnote.com", []string{params.Email}, "text/plain", body); err != nil {
 		HandleError(w, errors.Wrap(err, "queueing email").Error(), nil, http.StatusInternalServerError)
 	}
 }
