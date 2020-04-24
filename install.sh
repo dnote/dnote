6e041cdaa345df0f7bb84a682e3d764103324162
Sung Won Cho <sung@monomax.sh>
2020-04-24 15:30:55 +1000

    Build CLI for arm64

--- install.sh
+++ install.sh
@@ -70,6 +70,7 @@ uname_arch() {
   arch=$(uname -m)
   case $arch in 
     x86_64) arch="amd64" ;;
+    aarch64) arch="arm64" ;;
     x86) arch="386" ;;
     i686) arch="386" ;;
     i386) arch="386" ;;
