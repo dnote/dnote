1b7473149c76ba69d8f1587ed747ded82d757d9e
Sung Won Cho <hello@sungwoncho.com>
2021-01-03 11:37:34 +1100

    Update install_postgres.sh (#525)

--- scripts/vagrant/install_postgres.sh
+++ scripts/vagrant/install_postgres.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-sudo apt-get install wget ca-certificates
+sudo apt-get -y install wget ca-certificates
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
 
