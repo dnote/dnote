7074ea011843836dbd47e9f56fc88f8b43901a97
Sung Won Cho <sung@monomax.sh>
2022-02-12 09:10:43 +1100

    Fix build annotation

--- pkg/cli/dirs/dirs_unix_test.go
+++ pkg/cli/dirs/dirs_unix_test.go
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU General Public License
  * along with Dnote.  If not, see <https://www.gnu.org/licenses/>.
  */
-
 // +build linux darwin
 
 package dirs
