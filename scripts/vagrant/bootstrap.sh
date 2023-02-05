808a31bf549d0f1aab79a6f42ff5e9cabd03437b
Sung <sung@monomaxlabs.com>
2023-02-05 13:01:28 +1100

    Upgrade dependencies

--- scripts/vagrant/bootstrap.sh
+++ scripts/vagrant/bootstrap.sh
@@ -10,8 +10,10 @@ echo "cd /go/src/github.com/dnote/dnote" >> /home/vagrant/.bash_profile
 # set up database
 sudo -u postgres createdb dnote
 sudo -u postgres createdb dnote_test
+sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'postgres';"
+
 # allow connection from host and allow to connect without password
-sudo sed -i  "/port*/a listen_addresses = '*'" /etc/postgresql/11/main/postgresql.conf
-sudo sed -i 's/host.*all.*.all.*md5/# &/' /etc/postgresql/11/main/pg_hba.conf
-sudo sed -i "$ a host all all all trust" /etc/postgresql/11/main/pg_hba.conf
+sudo sed -i  "/port*/a listen_addresses = '*'" /etc/postgresql/14/main/postgresql.conf
+sudo sed -i 's/host.*all.*.all.*md5/# &/' /etc/postgresql/14/main/pg_hba.conf
+sudo sed -i "$ a host all all all trust" /etc/postgresql/14/main/pg_hba.conf
 sudo service postgresql restart
--- scripts/vagrant/install_postgres.sh
+++ scripts/vagrant/install_postgres.sh
@@ -6,4 +6,4 @@ wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
 
 sudo apt-get update
-sudo apt-get install -y postgresql-11
+sudo apt-get install -y postgresql-14
